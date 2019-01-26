63ea720a8db388091ea61cfbbd47ee7de6dbb792
Konrad Sedkowski <ksedkowski@gmail.com>
2019-01-26 11:09:07 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+      output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+            loader: 'babel-loader',
+            options: {
+              presets: ['react']
+            }
+          }
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+                modules: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
